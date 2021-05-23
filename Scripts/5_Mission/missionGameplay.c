90db1fa8142992a52f5c699174799d8e29f77c64
rootkt <64445926+rootkt@users.noreply.github.com>
2021-05-23 00:17:17 -0300

    Update missionGameplay.c

--- Scripts/5_Mission/missionGameplay.c
+++ Scripts/5_Mission/missionGameplay.c
@@ -2,12 +2,10 @@ modded class MissionGameplay
 {
     override void OnKeyPress(int key)
 	{
-		//Print("[STICKY] Apertou tecla");
 		super.OnKeyPress(key);
 			
 		if ( key == KeyCode.KC_K )
 		{
-			//Print("[STICKY] Apertou K");
 			toggleStickyGrenades();
         }
     }
