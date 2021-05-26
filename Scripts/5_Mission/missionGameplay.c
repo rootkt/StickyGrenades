1b4dcb5e95859c6addf4179b2d10d1922857001f
rootkt <64445926+rootkt@users.noreply.github.com>
2021-05-26 15:38:14 -0300

    Update missionGameplay.c

--- Scripts/5_Mission/missionGameplay.c
+++ Scripts/5_Mission/missionGameplay.c
@@ -1,12 +1,12 @@
 modded class MissionGameplay
 {
-    override void OnKeyPress(int key)
+	override void OnKeyPress(int key)
 	{
 		super.OnKeyPress(key);
-			
+		
 		if ( key == KeyCode.KC_K )
 		{
 			toggleStickyGrenades();
-        }
-    }
+		}
+	}
 }
